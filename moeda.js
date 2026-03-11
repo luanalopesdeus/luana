1111d498d6f7ad5a419fc224d9292e96a5bc658f
luanalopesdeus <luana.lopes.deus@escola.pr.gov.br>
2026-03-11 15:21:03 -0300

    Add files via upload

---
+++ moeda.js
@@ -0,0 +1,21 @@
+let moedasOuro = 150;
+let qPocao = 4;
+let vPocao = -30;
+
+let restoMoedas = (qPocao * vPocao) + moedasOuro;
+
+console . log("O restante das moedas é:" + restoMoedas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
