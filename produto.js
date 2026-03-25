a32808d99f3ef922c6e4f8c858fe1dfcc0180f36
luanalopesdeus <luana.lopes.deus@escola.pr.gov.br>
2026-03-25 18:31:37 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,14 @@
+const produto = "mochila Ipermeavel"
+let preco  = 189.90;
+let estoque = 14;
+const cateforia = "acessório";
+
+console.log(`O produto é : ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco} preco. toFixed(2)}.`);
+console.log (` o estoque ainda possui $(estoque)mochilas.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
+
+
+
+
+
