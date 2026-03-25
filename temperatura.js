ca4c094ab66212ee240a0a41f0bd4225177f5cb0
luanalopesdeus <luana.lopes.deus@escola.pr.gov.br>
2026-03-25 18:51:49 +0000

    versao3

--- temperatura.js
+++ temperatura.js
@@ -2,7 +2,10 @@
 const cidade = "Curitiba";
 let tempCelsius = 18.5;
 
+const tempfahrenheit = tempCelsius * (9/5) + 32;
+const tempkelvin = tempCelsius + 237.15;
 
-
-
+console.log(`A tempetura de ${cidade} está {tempcelsius} Cº`);
+console.log (`Fahrenheit: ${tempfahrenheit. toFixed}(1)} Fº`);
+console.log(`Kelvin: $ (tempKeven.toxed(1)} Kº `);
 
