eb8013e4626c1b106c0a0eb5ae07ef905e82483b
luanalopesdeus <luana.lopes.deus@escola.pr.gov.br>
2026-04-14 17:36:19 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,17 @@
+function subirDeNivel(nivelAtual){
+    return nivelAtual + 1;
+};
+
+let meuNivel = 10;
+let nivelAtulizado = subirDeNivel(meuNivel);
+
+console.log("Seu nivel atualb é: " + nivelAtulizado);
+
+
+
+
+
+
+
+
+
