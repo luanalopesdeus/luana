af0c2672c5d40b5118b9bf77eb5e9f25748c9675
luanalopesdeus <luana.lopes.deus@escola.pr.gov.br>
2026-05-11 19:11:15 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,7 @@
+const nome ="João";
+let cargo="Operador de caixa";
+let idade=45;
+let salario=1500.00;
+let empresa="Assai";
+
+console.log(`Qual seu nome? ${nome} Oque quer fazer na empresa? ${cargo} Qual sua idade? ${idade} Quanto quer receber? ${salario} Qual sua empresa? ${empresa}`);
